d796ec67faf5ea125a36b0944f845a5884cdf362
zwrck <156535103+zwrck@users.noreply.github.com>
2024-04-11 15:42:59 +0700

    Update 02-install.sh

--- 02-install.sh
+++ 02-install.sh
@@ -26,7 +26,6 @@ sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sed -i 's/^#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 >> /etc/locale.conf
-echo LANG=ru_RU.UTF-8 >> /etc/locale.conf
 
 # Setting hostname & username
 
