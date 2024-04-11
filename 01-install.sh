50238e53c227b7aca90126d131160c622b7d9016
zwrck <156535103+zwrck@users.noreply.github.com>
2024-04-11 13:34:08 +0700

    Add files via upload

---
+++ 01-install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+chmod +x ArchTSUAB/01-install.sh ArchTSUAB/02-install.sh
+
+echo -ne "
+████████╗███████╗██╗   ██╗ █████╗ ██████╗
+╚══██╔══╝██╔════╝██║   ██║██╔══██╗██╔══██╗
+   ██║   ███████╗██║   ██║███████║██████╔╝
+   ██║   ╚════██║██║   ██║██╔══██║██╔══██╗
+   ██║   ███████║╚██████╔╝██║  ██║██████╔╝
+   ╚═╝   ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝
+------------------------------------------
+Arch Installation Part One
+
+Setting up mirrors
+"
+
+timedatectl set-ntp 1
+
+iso=$(curl -4 ifconfig.co/country-iso)
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+
+# Table and Partitioning
+echo "Partitioning"
+
+sgdisk -a 2048 -o /dev/sda
+sgdisk -n 1::+1G --typecode=1:ef00 /dev/sda
+sgdisk -n 2::0 --typecode=2:8300 /dev/sda
+
+#sgdisk --change-name=1:'EFIBOOT' /dev/sda
+#sgdisk --change-name=2:'ROOT' /dev/sda
+
+# Formating
+echo "Formating"
+
+mkfs.ext4 /dev/sda2
+mkfs.fat -F 32 /dev/sda1
+
+# Mounting
+echo "Mounting"
+
+mount /dev/sda2 /mnt
+mount --mkdir /dev/sda1 /mnt/boot/
+
+# Install essential packages
+echo "Installing base system"
+
+pacstrap -K /mnt base linux linux-firmware nano sudo --noconfirm --needed;
+
+# Configure the system
+echo "Generating fstab"
+
+genfstab -U /mnt >> /mnt/etc/fstab
+cat /mnt/etc/fstab
+
+cp -R ArchTSUAB /mnt/home/
+
+echo -e "Proceed with Part Two\ncd /home/ArchTSUAB\n./02-install.sh"
+
+arch-chroot /mnt
---
+++ 02-install.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+echo -ne "
+████████╗███████╗██╗   ██╗ █████╗ ██████╗
+╚══██╔══╝██╔════╝██║   ██║██╔══██╗██╔══██╗
+   ██║   ███████╗██║   ██║███████║██████╔╝
+   ██║   ╚════██║██║   ██║██╔══██║██╔══██╗
+   ██║   ███████║╚██████╔╝██║  ██║██████╔╝
+   ╚═╝   ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝
+------------------------------------------
+Arch Installation Part Two
+
+Setting up locales
+"
+
+echo -n "Enter your Region and City"
+read region city
+ln -sf /usr/share/zoneinfo/$region/$city /etc/localtime
+hwclock --systohc
+
+# Generating locales
+
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/^#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 >> /etc/locale.conf
+echo LANG=ru_RU.UTF-8 >> /etc/locale.conf
+
+# Setting hostname & username
+
+echo -n "Create hostname:"
+read input
+echo $input >> /etc/hostname
+echo -n "hostname: "; cat /etc/hostname
+echo -n "Create root password: "
+passwd
+echo -n "Create username:"
+read input
+useradd -m -G wheel $input
+echo -ne "username: $input\n";
+echo "Create $input password:"
+passwd $input
+
+# Sudoers
+
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+# Installing microcode
+echo "Installing microcode"
+
+proc_type=$(lscpu)
+if grep -E "GenuineIntel" <<< ${proc_type}; then
+    echo "Installing Intel microcode"
+    pacman -S --noconfirm --needed intel-ucode
+elif grep -E "AuthenticAMD" <<< ${proc_type}; then
+    echo "Installing AMD microcode"
+    pacman -S --noconfirm --needed amd-ucode
+fi
+
+# Systemd-boot
+echo "Setting up Systemd-boot"
+
+bootctl install
+
+truncate --size=0 /boot/loader/loader.conf
+echo -e "default\t\tarch.conf\ntimeout\t\t0\nconsole-mode\tmax\neditor\t\tno" >> /boot/loader/loader.conf
+echo -e "title\tLinux\nlinux\t/vmlinuz-linux\ninitrd\t/initramfs-linux.img\noptions\troot=dev/sda2 rw quiet" >> /boot/loader/entries/arch.conf
+
+# Networking
+echo "Installing NetworkManager"
+
+pacman -S --noconfirm --needed networkmanager git
+systemctl enable NetworkManager
+
+# Rebooting
+echo "Rebooting in 3 Seconds ..." && sleep 1
+echo "Rebooting in 2 Seconds ..." && sleep 1
+echo "Rebooting in 1 Second ..." && sleep 1
+
+exit
+reboot
